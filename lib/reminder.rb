f1092db7519b3772f8a384278e5eb56f4463e9d5
raymond anderson <raymond.anderson84@hotmail.co.uk>
2023-03-06 12:17:48 +0000

    #Adding first bites

---
+++ lib/reminder.rb
@@ -0,0 +1,15 @@
+# File: lib/reminder.rb
+
+class Reminder
+    def initialize(name)
+      @name = name
+    end
+  
+    def remind_me_to(task)
+      @task = task
+    end
+  
+    def remind()
+      return "#{@task}, #{@name}!"
+    end
+  end
